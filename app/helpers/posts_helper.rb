ca62d7a8078ba4419c164e88389783bc0cdb3485
tianshuai <tianshuai@chinavisual.com>
2013-06-16 04:11:48 +0800

    add current_position

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,3 +1,19 @@
 # encoding: utf-8
 module PostsHelper
+
+  #当前位置判断（分类：mark）
+  def path_cate(post,cate)
+
+    if post.present?
+	  str = "#{link_to('学院新闻', posts_path)} > #{link_to(post.category.name, {action: 'list', mark: post.category.mark })} > #{post.title}"
+    else
+	  if cate.present?
+	    str = "#{link_to('学院新闻', posts_path)} > #{cate.name}"
+	  else
+	    str = "学院新闻"
+	  end
+    end
+	return str	
+  end
+
 end
